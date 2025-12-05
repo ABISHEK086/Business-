e0d68ec84f9aed9531818ba5755948041f727726
ABISHEK ANTONY <abishek8673@gmail.com>
2025-12-05 18:10:42 +0530

    Update FeaturesSection.jsx

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion"; 
 import { fadeIn, textVariant } from "../utils/motion";
- 
+  
 const FeaturesSection = () => {     
   const features = [     
     {      
