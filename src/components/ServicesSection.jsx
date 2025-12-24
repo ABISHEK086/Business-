c1bce6f391f1ba200c0cf54c6dc9497a3f085fbb
ABISHEK ANTONY <abishek8673@gmail.com>
2025-12-24 17:56:59 +0530

    Update ServicesSection.jsx

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BsStack } from 'react-icons/bs'
 import { HiLightBulb } from 'react-icons/hi'
 import { FiSettings } from 'react-icons/fi'
-import { BiTime } from 'react-icons/bi'
+import { BiTime } from 'react-icons/bi' 
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
  
