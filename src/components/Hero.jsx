5ba44f44ba1ee4578c69da2b8ad7b6343ade5ed7
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-23 16:00:10 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
-import heroImage from '../assets/hero-image.png' 
+import heroImage from '../assets/hero-image.png'  
 
 const Hero = () => { 
   return (
